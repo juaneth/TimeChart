82afb20484923a39b5b898a2d8ad18f3c1b0c22b
Juan <euanwalker2008@gmail.com>
2021-12-09 22:01:36 +0000

    worked on api

--- admin/api/api.js
+++ admin/api/api.js
@@ -2,13 +2,30 @@ const express = require('express');
 const fs = require('fs');
 const workerlist = require('./workerlist.json');
 
-function log(type, content) {
+function log(type, content, address) {
     if (type == 'error') {
         console.error(content);
-        fs.writeFileSync();
+        fs.writeFileSync("errors.log", type + ': ' + content);
     }
 
     if (type == 'log') {
-        console.log(content);
+        console.log(`::${content}::`);
+        var prev = fs.readFileSync("logs.log", "utf8");
+        if (address = undefined) {
+
+        }
+        fs.writeFileSync("logs.log", `${prev}\n${type}, IP: ${address}: ${content}`);
     }
 }
+
+const app = express()
+const port = 3000
+
+app.get('/', (req, res) => {
+    res.send('Hello World!')
+    log("log", "Main Requested");
+})
+
+app.listen(port, () => {
+    log('log', `API listening at http://localhost:${port}`);
+})
